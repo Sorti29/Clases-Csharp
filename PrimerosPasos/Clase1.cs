501b11ff8bb932d30cc4c8fde11dfda88dc2de34
Sorti29 <gvidaurrerojas@gmail.com>
2024-09-11 23:00:37 -0300

    Mitad de Primera Clase

--- PrimerosPasos/Clase1.cs
+++ PrimerosPasos/Clase1.cs
@@ -1,3 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Prueba2");
+
+/*
+// Calculadora Resuelve si el Numero ingresado es par o impar
+
+Console.WriteLine("Ingrese un Numero Entero");
+
+int NumeroParImpar = Convert.ToInt32(Console.ReadLine());
+
+float resta = NumeroParImpar % 2;
+
+Console.WriteLine(resta);
+
+if (resta == 0)
+    Console.WriteLine("TU numero es par");
+
+else Console.WriteLine("Tu numero es Impar");
+
+*/
+
+// Actividad 1
+
